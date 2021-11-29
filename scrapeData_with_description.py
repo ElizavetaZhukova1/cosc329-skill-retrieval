14a8c530b26b286e2d223ee4a0da06b1170d57f6
sophiaj5 <59673737+sophiaj5@users.noreply.github.com>
2021-11-28 19:49:12 -0800

    added to .csv file

--- scrapeData_with_description.py
+++ scrapeData_with_description.py
@@ -9,13 +9,25 @@ from time import sleep
 import csv
 from random import randint 
 
-postings = [] 
+postings = []
+def get_description(jobLink):
+    response = requests.get(jobLink)
+    soup = BeautifulSoup(response.content, "html.parser")
+    description=soup.find('div', 'jobsearch-jobDescriptionText')
+    if description is None:
+        return ''
+    return description.text
+
 
 def getPosts(pos, loc, province, page):
     url = (f'https://ca.indeed.com/jobs?q={pos}&l={loc}%2C+{province}&start={page}')
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    posts = soup.find_all('div', class_ = 'job_seen_beacon')
+    all_posts = soup.find_all('div', class_='mosaic mosaic-provider-jobcards')
+    if not all_posts:
+        return
+    posts = all_posts[0]
+    posts = posts.find_all(class_='result')
     for item in posts:
         info = {
         #getting the title
@@ -25,23 +37,23 @@ def getPosts(pos, loc, province, page):
         #getting the location
         'location': item.find('div', class_ = 'companyLocation').text,
         #getting the job description
-        'description': item.find('div', class_ = 'job-snippet').text,
-        #getting more description
-        'more': item.find('div', class_='jobsearch-JobComponent-embeddedBody').text,
+        #'description': item.find('div', class_ = 'job-snippet').text,
+        'description': get_description('https://www.indeed.com'+item.get('href')),
         #getting the post date
         'postDate': item.find('span', 'date').text,
         #the name of position
         'position': pos,
         }
-        postings.append(info)
-    return
+        if info['description']: #If there's something in the description, add the job to the dataset
+            postings.append(info)
 
-for i in range(1, 2):
+for i in range(6, 11):
     getPosts('developer', 'Toronto', 'ON', i)
-    #getPosts('manager', 'Toronto', 'ON', i)
-    time.sleep(5)
+    time.sleep(1)
+    getPosts('manager', 'Toronto', 'ON', i)
+    time.sleep(1)
 
-#print(len(postings))
+print(len(postings))
 try:
     old_df=pd.read_csv('jobs.csv')
     new_df=pd.DataFrame(postings)
@@ -51,6 +63,7 @@ except pd.errors.EmptyDataError as err:
 
 final_df.to_csv('jobs.csv')
 
-
-#jobs = pd.DataFrame(postings)
-#jobs.to_csv('jobs.csv')
+'''
+jobs = pd.DataFrame(postings)
+jobs.to_csv('jobs.csv')
+'''
